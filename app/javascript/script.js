99dc2523915a3e7770ecbbf63c4fd94fd30601c3
Ubuntu <281125600+GH3stu@users.noreply.github.com>
2026-06-10 13:19:42 +0000

    fix:swiper_app_jQueryを導入まで

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,2 +1,7 @@
-let title = "javascriptが使えました";
-alert(title)
+import $ from "jquery";
+
+document.addEventListener("turbo:load", () => {
+  $('.jquery').on('click', function () {
+    $(this).css('color', 'red');
+  });
+});
